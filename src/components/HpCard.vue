bde36e8e74e89966cae641e3819a9b36709d59c2
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-09-17 00:24:33 +0200

    implement hp card

---
+++ src/components/HpCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-card class="row q-gutter-x-lg items-center q-pr-lg q-py-sm col">
+    <div class="column">
+      <div>DC</div>
+      <div>14</div>
+    </div>
+    <q-linear-progress :value="hpRatio" color="primary" class="col" style="height: 15px">
+      <div class="absolute-full flex flex-center text-white text-caption" style="line-height: 15px">
+        {{ currentHp }} / {{ maxHp }}
+      </div>
+    </q-linear-progress>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HpCard',
+
+  data() {
+    return {
+      maxHp: 22,
+      currentHp: 14,
+    };
+  },
+
+  computed: {
+    hpRatio(): number {
+      console.log('ratio: ', this.currentHp / this.maxHp);
+      return this.currentHp / this.maxHp;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.hp-bar {
+  width: 200px;
+  height: 15px;
+}
+</style>
