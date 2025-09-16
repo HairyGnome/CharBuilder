10152856d31fb7bde873ce2f1ca1603381fad360
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-09-17 00:24:42 +0200

    implement level card

---
+++ src/components/LevelCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-card class="row q-py-sm q-col-gutter-x-md q-pr-md">
+    <q-input label="Character Name" v-model="characterName" dense class="col" />
+    <q-input label="Level" v-model="level" dense class="col-3" />
+    <q-input label="XP" v-model="xp" dense class="col-3" />
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LevelCard',
+
+  data() {
+    return {
+      characterName: 'Unknown Adventurer',
+      level: 1,
+      xp: 0,
+    };
+  },
+});
+</script>
