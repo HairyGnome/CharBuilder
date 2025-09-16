c88a12ac9f9f42449569967d666301715d3731e2
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-09-17 00:25:13 +0200

    add implemented cards

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,45 +1,73 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <div class="text-h1">Something</div>
+    <q-card class="q-pa-sm q-ml-lg">
+      <q-scroll-area style="height: 80vh; width: 300px">
+        <q-input class="col" v-for="i in 100" :key="i" v-model="model" />
+      </q-scroll-area>
+    </q-card>
+    <div class="column col q-px-xl">
+      <div class="row items-center q-gutter-x-lg">
+        <level-card />
+        <ability-score-card
+          class="col"
+          :stats="[
+            {
+              name: 'SIZE',
+              value: 'Medium',
+            },
+            {
+              name: 'SPEED',
+              value: '35ft.',
+            },
+            {
+              name: 'STR',
+              value: '+3',
+            },
+            {
+              name: 'DEX',
+              value: '+1',
+            },
+            {
+              name: 'CON',
+              value: '+1',
+            },
+            {
+              name: 'INT',
+              value: '+0',
+            },
+            {
+              name: 'WIS',
+              value: '+0',
+            },
+            {
+              name: 'CHA',
+              value: '+0',
+            },
+          ]"
+        />
+        <hp-card />
+      </div>
+      <gear-card class="q-mt-lg" />
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
+import AbilityScoreCard from 'src/components/AbilityScoreCard.vue';
+import GearCard from 'src/components/GearCard.vue';
+import HpCard from 'src/components/HpCard.vue';
+import LevelCard from 'src/components/LevelCard.vue';
 import { defineComponent } from 'vue';
-import type { Todo, Meta } from 'components/models';
 
 export default defineComponent({
   name: 'IndexPage',
 
+  components: { AbilityScoreCard, HpCard, GearCard, LevelCard },
+
   data() {
-    const todos: Todo[] = [
-      {
-        id: 1,
-        content: 'ct1',
-      },
-      {
-        id: 2,
-        content: 'ct2',
-      },
-      {
-        id: 3,
-        content: 'ct3',
-      },
-      {
-        id: 4,
-        content: 'ct4',
-      },
-      {
-        id: 5,
-        content: 'ct5',
-      },
-    ];
-
-    const meta: Meta = {
-      totalCount: 1200,
+    return {
+      model: 'hello',
     };
-
-    return { todos, meta };
   },
 });
 </script>
