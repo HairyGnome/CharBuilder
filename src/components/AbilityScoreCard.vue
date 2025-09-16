af2e14198d33c2e143e2ccfc7ee97cc7eb78923d
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-09-17 00:24:20 +0200

    implement ability score card

---
+++ src/components/AbilityScoreCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-card class="row q-gutter-x-lg q-pr-lg q-py-sm justify-center col-auto">
+    <div class="column items-center" v-for="(stat, idx) in stats" :key="idx">
+      <div class="row justify-center">
+        <q-img v-if="stat.icon" :src="stat.icon" />
+        <span>{{ stat.name }}</span>
+      </div>
+      <div>{{ stat.value }}</div>
+    </div>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { Stat } from '../types.js';
+
+export default defineComponent({
+  name: 'AbilityScoreCard',
+
+  props: {
+    stats: {
+      type: Array<Stat>,
+      required: true,
+    },
+  },
+});
+</script>
