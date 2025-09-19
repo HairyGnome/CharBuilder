e2b409cb245bce639a55fa61b97b945aef1156fd
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-09-19 22:51:26 +0200

    create characterstore

---
+++ src/stores/character_store.ts
@@ -0,0 +1,121 @@
+import { defineStore } from 'pinia';
+import type { CharacterState } from 'src/models/types';
+import { DamageTypes } from 'src/models/weapon_types';
+
+export const useCharacterStore = defineStore('characterStore', {
+  state(): CharacterState {
+    return {
+      name: 'Unknown advernturer',
+      ancestry: 'Human',
+      size: 'medium',
+      speed: 25,
+      abilityScores: {
+        str: 16,
+        dex: 14,
+        con: 14,
+        int: 12,
+        wis: 10,
+        cha: 10,
+      },
+      dc: 14,
+      hp: {
+        currentHp: 14,
+        maxHp: 22,
+        tempHp: 0,
+      },
+      weapons: [
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'crowssow, heavy',
+          damage: { amount: { amount: 1, type: 10 }, type: DamageTypes.PIERCING },
+          bulk: 2,
+          properties: ['projectile (range 100/400)', 'loading'],
+          heft: 4,
+          special: 'Braced fire',
+          mastery: 'Fast hands',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'crowssow, heavy',
+          damage: { amount: { amount: 1, type: 10 }, type: DamageTypes.PIERCING },
+          bulk: 2,
+          properties: ['projectile (range 100/400)', 'loading'],
+          heft: 4,
+          special: 'Braced fire',
+          mastery: 'Fast hands',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'crowssow, heavy',
+          damage: { amount: { amount: 1, type: 10 }, type: DamageTypes.PIERCING },
+          bulk: 2,
+          properties: ['projectile (range 100/400)', 'loading'],
+          heft: 4,
+          special: 'Braced fire',
+          mastery: 'Fast hands',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+      ],
+    };
+  },
+
+  getters: {
+    getAbilityScoreModifier:
+      (state: CharacterState) =>
+      (ability: keyof CharacterState['abilityScores']): number => {
+        return Math.floor((state.abilityScores[ability] - 10) / 2);
+      },
+  },
+
+  actions: {},
+});
