741551615241ea51d62cadc3a893e064600654a4
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-09-19 22:54:26 +0200

    add cards to index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,53 +1,19 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <q-card class="q-pa-sm q-ml-lg">
-      <q-scroll-area style="height: 80vh; width: 300px">
-        <q-input class="col" v-for="i in 100" :key="i" v-model="model" />
-      </q-scroll-area>
-    </q-card>
-    <div class="column col q-px-xl">
-      <div class="row items-center q-gutter-x-lg">
-        <level-card />
-        <ability-score-card
-          class="col"
-          :stats="[
-            {
-              name: 'SIZE',
-              value: 'Medium',
-            },
-            {
-              name: 'SPEED',
-              value: '35ft.',
-            },
-            {
-              name: 'STR',
-              value: '+3',
-            },
-            {
-              name: 'DEX',
-              value: '+1',
-            },
-            {
-              name: 'CON',
-              value: '+1',
-            },
-            {
-              name: 'INT',
-              value: '+0',
-            },
-            {
-              name: 'WIS',
-              value: '+0',
-            },
-            {
-              name: 'CHA',
-              value: '+0',
-            },
-          ]"
-        />
-        <hp-card />
+  <q-page>
+    <div class="row items-stretch justify-evenly q-pt-xl" style="height: 90vh">
+      <q-card class="q-pa-sm q-ml-lg">
+        <q-scroll-area style="height: 100%; width: 300px">
+          <class-select-card />
+        </q-scroll-area>
+      </q-card>
+      <div class="column col q-px-xl">
+        <div class="row items-center q-gutter-x-lg">
+          <level-card />
+          <ability-score-card class="col" />
+          <hp-card />
+        </div>
+        <gear-card class="q-mt-lg col-grow" />
       </div>
-      <gear-card class="q-mt-lg" />
     </div>
   </q-page>
 </template>
@@ -57,17 +23,12 @@ import AbilityScoreCard from 'src/components/AbilityScoreCard.vue';
 import GearCard from 'src/components/GearCard.vue';
 import HpCard from 'src/components/HpCard.vue';
 import LevelCard from 'src/components/LevelCard.vue';
+import ClassSelectCard from 'src/components/class-cards/ClassSelectCard.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
 
-  components: { AbilityScoreCard, HpCard, GearCard, LevelCard },
-
-  data() {
-    return {
-      model: 'hello',
-    };
-  },
+  components: { AbilityScoreCard, HpCard, GearCard, LevelCard, ClassSelectCard },
 });
 </script>
