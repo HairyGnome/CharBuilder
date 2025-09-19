b8e66a29093089334f4d78f59cc88742a1b1f285
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-09-19 22:52:25 +0200

    implement weapons page

---
+++ src/components/gear-pages/weapons-page/WeaponCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-card class="column q-px-md q-my-md" bordered flat>
+    <q-card-section class="row justify-between text-center">
+      <div class="col">{{ weapon.name.toUpperCase() }}</div>
+      <div class="col">
+        {{ weapon.damage.amount.amount }}d{{ weapon.damage.amount.type }}
+        {{ weapon.damage.type.toUpperCase() }}
+      </div>
+      <div class="col">BULK: {{ weapon.bulk }}</div>
+      <div class="col">
+        {{
+          weapon.properties.length > 0
+            ? weapon.properties.map((p) => p.toUpperCase()).join(', ')
+            : '-'
+        }}
+      </div>
+      <div class="col">HEFT: {{ weapon.heft }}</div>
+      <div class="col">{{ weapon.special }}</div>
+      <div class="col">{{ weapon.mastery }}</div>
+    </q-card-section>
+    <q-card-section class="row"></q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { Weapon } from 'src/models/weapon_types';
+
+export default defineComponent({
+  name: 'WeaponCard',
+
+  props: {
+    weapon: {
+      type: Object as () => Weapon,
+      required: true,
+    },
+  },
+});
+</script>
---
+++ src/components/gear-pages/weapons-page/WeaponsPage.vue
@@ -0,0 +1,108 @@
+<template>
+  <q-card-section style="height: 100%">
+    <q-scroll-area class="column" style="height: 100%">
+      <weapon-card v-for="(weapon, idx) in weapons" :key="idx" :weapon="weapon" />
+    </q-scroll-area>
+  </q-card-section>
+</template>
+
+<script lang="ts">
+import type { Weapon } from 'src/models/weapon_types';
+import { DamageTypes } from 'src/models/weapon_types';
+import { defineComponent } from 'vue';
+import WeaponCard from './WeaponCard.vue';
+
+export default defineComponent({
+  name: 'WeaponsPage',
+
+  components: { WeaponCard },
+
+  data() {
+    return {
+      weapons: [
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'crowssow, heavy',
+          damage: { amount: { amount: 1, type: 10 }, type: DamageTypes.PIERCING },
+          bulk: 2,
+          properties: ['projectile (range 100/400)', 'loading'],
+          heft: 4,
+          special: 'Braced fire',
+          mastery: 'Fast hands',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'crowssow, heavy',
+          damage: { amount: { amount: 1, type: 10 }, type: DamageTypes.PIERCING },
+          bulk: 2,
+          properties: ['projectile (range 100/400)', 'loading'],
+          heft: 4,
+          special: 'Braced fire',
+          mastery: 'Fast hands',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+        {
+          name: 'crowssow, heavy',
+          damage: { amount: { amount: 1, type: 10 }, type: DamageTypes.PIERCING },
+          bulk: 2,
+          properties: ['projectile (range 100/400)', 'loading'],
+          heft: 4,
+          special: 'Braced fire',
+          mastery: 'Fast hands',
+        },
+        {
+          name: 'longsword',
+          damage: { amount: { amount: 1, type: 8 }, type: DamageTypes.SLASHING },
+          bulk: 1,
+          properties: [],
+          heft: 4,
+          special: 'Quick parry',
+          mastery: 'Prince slayer',
+        },
+      ] as Weapon[],
+    };
+  },
+});
+</script>
