ec3a04c696943ba71cd078755239c38a443a6e56
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-09-23 01:41:31 +0200

    add dialog for base ability scores

--- src/components/class-cards/ClassSelectCard.vue
+++ src/components/class-cards/ClassSelectCard.vue
@@ -1,39 +1,20 @@
 <template>
+  <div class="text-subtitle1 q-mt-lg">Class</div>
+  <q-separator class="q-mb-md" />
   <q-card-section class="column q-gutter-y-md">
-    <div class="text-subtitle1">Ancestry</div>
-    <q-select
-      v-model="selectedAncestry"
-      :options="ancestryLabels"
-      label="Select Ancestry"
-      dense
-      outlined
-      class="col q-mt-none"
-    />
-    <div class="text-subtitle1">Lineage</div>
-    <q-select
-      v-model="selectedLineage"
-      :options="lineageLabels"
-      dense
-      outlined
-      class="col q-mt-none"
-    />
-  </q-card-section>
-  <q-separator class="q-my-md" />
-  <q-card-section class="column q-gutter-y-md">
-    <div class="text-subtitle1">Class</div>
     <q-select
       v-model="selectedClass"
       :options="classLabels"
-      label="Select Class"
+      label="Class"
       dense
       outlined
       class="col q-mt-none"
     />
-    <div class="text-subtitle1">Class Roles</div>
-    <div class="row q-gutter-x-sm q-mt-none">
+    <div class="row q-gutter-x-sm">
       <q-input
         v-for="(role, idx) in classRoles"
         :model-value="role.toUpperCase()"
+        :label="`Role ${idx + 1}`"
         dense
         outlined
         readonly
@@ -42,18 +23,71 @@
       />
     </div>
   </q-card-section>
+  <div class="text-subtitle1 q-mt-lg">Ancestry</div>
+  <q-separator class="q-mb-md" />
+  <q-card-section class="column q-gutter-y-md">
+    <q-select
+      v-model="selectedAncestry"
+      :options="ancestryLabels"
+      label="Ancestry"
+      dense
+      outlined
+      class="col q-mt-none"
+    />
+    <q-select
+      v-model="selectedLineage"
+      :options="lineageLabels"
+      label="Lineage"
+      dense
+      outlined
+      class="col"
+    />
+    <q-input
+      v-model="ancestryFeature"
+      label="Ancestry Feature"
+      dense
+      outlined
+      readonly
+      class="col"
+    />
+    <q-input
+      v-for="(passive, idx) in rolePassives"
+      :model-value="passive"
+      :label="`Role Passive ${idx + 1}`"
+      dense
+      outlined
+      readonly
+      :key="idx"
+      class="col"
+    />
+  </q-card-section>
+  <div class="text-subtitle1 q-mt-lg">Level 1</div>
+  <q-separator class="q-mb-lg" />
+  <q-card-section class="column">
+    <q-btn label="Select ability scores" @click="openAbilityScoreArrayDialog" />
+    <ability-score-array-dialog v-model="showAbilityScoreArrayDialog" />
+  </q-card-section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useDataStore } from 'src/stores/data-store';
 import { useCharacterStore } from 'src/stores/character_store';
+import AbilityScoreArrayDialog from '../dialogs/AbilityScoreArrayDialog.vue';
 
 const dataStore = useDataStore();
 const characterStore = useCharacterStore();
 
 export default defineComponent({
   name: 'ClassSelectCard',
+
+  components: { AbilityScoreArrayDialog },
+
+  data() {
+    return {
+      showAbilityScoreArrayDialog: false,
+    };
+  },
 
   computed: {
     classLabels() {
@@ -74,6 +108,20 @@ export default defineComponent({
 
     classRoles() {
       return dataStore.classes[characterStore.class]?.roles || [];
+    },
+
+    ancestryFeature() {
+      return (
+        dataStore.ancestries[characterStore.ancestry.ancestry]?.feature.unslugify().capitalize() ||
+        'N/A'
+      );
+    },
+
+    rolePassives(): string[] {
+      const roles = dataStore.classes[characterStore.class]?.roles || [];
+      const rolePassives: { [key: string]: string } =
+        dataStore.ancestries[characterStore.ancestry.ancestry]?.rolePassives || {};
+      return roles.map((role) => rolePassives[role]?.unslugify().capitalize() || 'N/A');
     },
 
     selectedClass: {
@@ -102,6 +150,12 @@ export default defineComponent({
       set(value: string) {
         characterStore.setLineage(value);
       },
+    },
+  },
+
+  methods: {
+    openAbilityScoreArrayDialog() {
+      this.showAbilityScoreArrayDialog = true;
     },
   },
 });
---
+++ src/components/dialogs/AbilityScoreArrayDialog.vue
@@ -0,0 +1,160 @@
+<template>
+  <q-dialog>
+    <q-card style="max-width: 800px; min-width: 765px">
+      <q-card-section class="text-h5">
+        <div>Select Ability Score Array</div>
+        <q-separator />
+      </q-card-section>
+      <q-card-section class="q-ml-lg q-mr-lg">
+        <div class="row justify-start">
+          <q-select
+            v-model="selectedAbilityScoreArray"
+            :options="abilityScoreArrays"
+            label="Select Array"
+            outlined
+            dense
+            class="col-4"
+          />
+        </div>
+        <div class="row justify-evenly q-mt-xl">
+          <table class="q-table q-mx-auto" style="width: 100%">
+            <thead>
+              <tr>
+                <th
+                  v-for="(name, idx) in ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']"
+                  :key="idx"
+                  class="text-center col"
+                >
+                  {{ name }}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td
+                  colspan="1"
+                  class="text-center col"
+                  v-for="(score, idx) in actualAbilityScoreArray"
+                  :key="idx"
+                  style="width: 1%"
+                >
+                  <div class="row justify-center items-center q-col-gutter-x-md">
+                    <q-icon
+                      :name="idx > 0 ? 'chevron_left' : ''"
+                      @click="moveToLeft(idx)"
+                      class="col"
+                      size="md"
+                    />
+                    <q-input
+                      :model-value="score"
+                      outlined
+                      readonly
+                      input-class="text-center"
+                      class="col-8"
+                    />
+                    <q-icon
+                      :name="idx < actualAbilityScoreArray.length - 1 ? 'chevron_right' : ''"
+                      @click="moveToRight(idx)"
+                      class="col"
+                      size="md"
+                    />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn label="Save" color="primary" @click="saveAbilityScoreArray" />
+        <q-btn label="Cancel" color="primary" @click="$emit('update:modelValue', false)" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import type { AbilityScores } from 'src/models/types';
+import { useCharacterStore } from 'src/stores/character_store';
+import { defineComponent } from 'vue';
+
+const characterStore = useCharacterStore();
+
+export default defineComponent({
+  name: 'AbilityScoreArrayDialog',
+
+  props: {},
+
+  data() {
+    return {
+      selectedAbilityScoreArray: 'Specialist',
+      abilityScoreArrays: ['Specialist', 'Balanced', 'Generalist'],
+      abilityScoreArray: {
+        specialist: [16, 12, 12, 10, 10, 8],
+        balanced: [14, 14, 12, 12, 10, 10],
+        generalist: [14, 12, 12, 12, 12, 12],
+      },
+    };
+  },
+
+  computed: {
+    actualAbilityScoreArray(): number[] {
+      let array: number[] = [];
+      switch (this.selectedAbilityScoreArray) {
+        case 'Specialist':
+          array = this.abilityScoreArray.specialist;
+          break;
+        case 'Balanced':
+          array = this.abilityScoreArray.balanced;
+          break;
+        case 'Generalist':
+          array = this.abilityScoreArray.generalist;
+          break;
+        default:
+          break;
+      }
+      return array;
+    },
+  },
+
+  methods: {
+    moveToRight(idx: number) {
+      if (
+        idx < this.actualAbilityScoreArray.length - 1 &&
+        typeof this.actualAbilityScoreArray[idx] === 'number' &&
+        typeof this.actualAbilityScoreArray[idx + 1] === 'number'
+      ) {
+        [this.actualAbilityScoreArray[idx], this.actualAbilityScoreArray[idx + 1]] = [
+          this.actualAbilityScoreArray[idx + 1] as number,
+          this.actualAbilityScoreArray[idx],
+        ];
+      }
+    },
+
+    moveToLeft(idx: number) {
+      if (
+        idx > 0 &&
+        typeof this.actualAbilityScoreArray[idx] === 'number' &&
+        typeof this.actualAbilityScoreArray[idx - 1] === 'number'
+      ) {
+        [this.actualAbilityScoreArray[idx], this.actualAbilityScoreArray[idx - 1]] = [
+          this.actualAbilityScoreArray[idx - 1] as number,
+          this.actualAbilityScoreArray[idx],
+        ];
+      }
+    },
+
+    saveAbilityScoreArray() {
+      characterStore.setAbilityScores({
+        str: this.actualAbilityScoreArray[0] || 0,
+        dex: this.actualAbilityScoreArray[1] || 0,
+        con: this.actualAbilityScoreArray[2] || 0,
+        int: this.actualAbilityScoreArray[3] || 0,
+        wis: this.actualAbilityScoreArray[4] || 0,
+        cha: this.actualAbilityScoreArray[5] || 0,
+      } as AbilityScores);
+      this.$emit('update:modelValue', false);
+    },
+  },
+});
+</script>
