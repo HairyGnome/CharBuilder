822dc3e78e19444b6ecea9ee17fc3204fe2a7155
HairyGnome <HairyGnome@users.noreply.github.com>
2025-10-18 16:08:21 +0000

    deploy: b03a113de3afa6f287f37dbd7fe23f70b85d1363

---
+++ assets/boot_extensions-DFKv1bIb.js
@@ -0,0 +1,1 @@
+String.prototype.capitalize=function(){return this.charAt(0).toUpperCase()+this.slice(1)};String.prototype.unslugify=function(){return this.replaceAll("_"," ")};String.prototype.slugify=function(){return this.replaceAll(" ","_")};
