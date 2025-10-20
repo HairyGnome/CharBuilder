0edaba79b6845d3e81148f23d803262d90fdcd50
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2025-10-20 22:59:17 +0200

    add transaction coloring

--- src/components/gear-pages/purse-page/TransactionHistoryItem.vue
+++ src/components/gear-pages/purse-page/TransactionHistoryItem.vue
@@ -6,19 +6,49 @@
     <span class="text-h6 col-4"> {{ transaction.label }} </span>
     <span class="col text-h6">
       <q-icon name="currency_exchange" style="color: #d4af37" class="q-mr-sm" />
-      <span class="text-right" style="display: inline-block; min-width: 60px">
+      <span
+        class="text-right"
+        style="display: inline-block; min-width: 60px"
+        :class="
+          transaction.goldChange < 0
+            ? 'text-negative'
+            : transaction.goldChange > 0
+              ? 'text-positive'
+              : ''
+        "
+      >
         {{ transaction.goldChange }}
       </span>
     </span>
     <span class="text-h6 col">
       <q-icon name="currency_exchange" style="color: #c0c0c0" class="q-mr-sm" />
-      <span class="text-right col" style="display: inline-block; min-width: 60px">
+      <span
+        class="text-right col"
+        style="display: inline-block; min-width: 60px"
+        :class="
+          transaction.silverChange < 0
+            ? 'text-negative'
+            : transaction.silverChange > 0
+              ? 'text-positive'
+              : ''
+        "
+      >
         {{ transaction.silverChange }}
       </span>
     </span>
     <span class="text-h6 col">
       <q-icon name="currency_exchange" style="color: #cd7f32" class="q-mr-sm" />
-      <span class="text-right col" style="display: inline-block; min-width: 60px">
+      <span
+        class="text-right col"
+        style="display: inline-block; min-width: 60px"
+        :class="
+          transaction.copperChange < 0
+            ? 'text-negative'
+            : transaction.copperChange > 0
+              ? 'text-positive'
+              : ''
+        "
+      >
         {{ transaction.copperChange }}
       </span>
     </span>
