33b68690349bf0d6c5b1ff8e8990b5c920d91e35
Arató Bence Bálint <76455572+HairyGnome@users.noreply.github.com>
2026-01-31 17:24:45 +0100

    add weapon page header

--- src/components/gear-pages/weapons-page/WeaponsPage.vue
+++ src/components/gear-pages/weapons-page/WeaponsPage.vue
@@ -1,10 +1,36 @@
 <template>
-  <q-card-section style="height: 100%">
-    <q-scroll-area class="column" style="height: 100%">
+  <q-card-section class="weapons-page">
+    <q-card class="q-px-md" flat>
+      <q-card-section class="row justify-between text-center text-h5">
+        <div class="col">Name</div>
+        <div class="col">Damage</div>
+        <div class="col">Bulk</div>
+        <div class="col">Properties</div>
+        <div class="col">Heft</div>
+        <div class="col">Special</div>
+        <div class="col">Mastery</div>
+      </q-card-section>
+    </q-card>
+    <q-separator />
+    <q-scroll-area class="weapons-scroll column">
       <weapon-card v-for="(weapon, idx) in weapons" :key="idx" :weaponName="weapon" />
     </q-scroll-area>
   </q-card-section>
 </template>
+
+<style scoped>
+.weapons-page {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.weapons-scroll {
+  flex: 1 1 auto;
+  min-height: 0;
+}
+</style>
 
 <script lang="ts">
 import { defineComponent } from "vue";
